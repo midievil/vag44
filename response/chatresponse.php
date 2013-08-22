7bc36073bd4a96fc9883cae6fa8c12f4d8b1bbcb
Artem Sharypow <artemiogro@gmail.com>
2013-08-23 01:02:05 +0400

    Lower traffic
    - Not allowing check too often

--- response/chatresponse.php
+++ response/chatresponse.php
@@ -29,37 +29,53 @@
     $currentUser = User::CurrentUser();
     
     //static $lastChatUpdate = '1';
+    $minInterval = 10;
     
     switch($_REQUEST["action"])
     {
         case "writemessage":            
             ChatDB::AddMessage(-1, User::CurrentUserID(), $_REQUEST['message'], DateFunctions::getCurrentDateText());
+            $_SESSION['lastChatUpdate'] = time() - $minInterval;
             return;
             
         case "getmessages":
         	
         	$result = '';
         	
-        	//$lastChatUpdate++;
+        	$lastChatUpdate = $_SESSION['lastChatUpdate'];
         	
-        	$res['lastupdate'] = $lastChatUpdate;
+        	if($lastChatUpdate == null)
+        	{
+        		$lastChatUpdate = time() - $minInterval;
+        	}
         	
-        	$messages = ChatDB::GetMessages(-1, $_REQUEST['fromid'], $_REQUEST['top']);
-            if(count($messages) > 0)
-            {
-                foreach($messages as $message){
-                    $result .= RenderFunctions::RenderChatMessage($message, $currentUser->CompactChat);
-                }
-                $res['html'] = $result;
-                $res['lastid'] = $messages[count($messages) -1]['ID'];
-                $res['firstid'] = $messages[0]['ID'];                
-            }
-            else {
-                $res['lastid'] = 'nomessages';
-                $res['html'] = '';
-            }
+        	$interval = time() - $lastChatUpdate;
+        	$res['interval'] = $interval; 
+        	
+        	if($interval >= $minInterval)
+        	{        		
+	        	$messages = ChatDB::GetMessages(-1, $_REQUEST['fromid'], $_REQUEST['top']);
+	            if(count($messages) > 0)
+	            {
+	                foreach($messages as $message){
+	                    $result .= RenderFunctions::RenderChatMessage($message, $currentUser->CompactChat);
+	                }
+	                $res['html'] = $result;
+	                $res['lastid'] = $messages[count($messages) -1]['ID'];
+	                $res['firstid'] = $messages[0]['ID'];
+	                echo json_encode($res);
+	                $_SESSION['lastChatUpdate'] = time();
+	                die;
+	            }
+        	}
             
+        	$res['lastid'] = 'nomessages';
+            $res['html'] = '';
             echo json_encode($res);
+            
+            
+            
+            
             
             return;
             
